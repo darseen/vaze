6191020ea851fdd87326d2179160a3decfd04aa5
darseen <darseen.dev@gmail.com>
2025-09-25 15:06:35 +0300

    service: fix docker hostname

--- apps/service/entrypoint.sh
+++ apps/service/entrypoint.sh
@@ -14,7 +14,8 @@ fi
 # Check for BASE_URL, default if not set
 if [ -z "${BASE_URL}" ]; then
   echo ">>> BASE_URL is not set. Defaulting to http://127.0.0.1:3000"
-  export BASE_URL="http://127.0.0.1:3000"
+  SERVICE_NAME=$(hostname)
+  export BASE_URL="http://${SERVICE_NAME}:3000"
 else
   echo ">>> Using existing BASE_URL: ${BASE_URL}"
 fi
